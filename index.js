2cc9add708c44056deaea67e542feaa2943b1dc0
longcao <longcao@localhost.localdomain>
2019-12-31 16:32:44 +0700

    let dog eat cat

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var Dog = require('./dog');
+var Cat = require('./cat');
+var Mouse = require('./mouse');
 
 var dog1 = new Dog('tom')
+var tom = new Cat();
+var mickey = new Mouse('mickey');
+mickey.die();
 
-dog1.sayhi();
+tom.eat(mickey)
+
+dog1.eat(tom);
+console.log(dog1)
