a2ac6d60b3d39def35dd009321d19f54d3744eb4
longcao <longcao@localhost.localdomain>
2020-01-01 09:55:52 +0700

    Improve cat eat method

--- cat.js
+++ cat.js
@@ -1,9 +1,14 @@
+var Mouse = require('./mouse');
 function cat(){
 	this.stomach = [];
 }
 
-cat.prototype.eat = function(mouse) {
-	this.stomach.push(mouse);
+cat.prototype.eat = function(animal) {
+	if (animal instanceof Mouse) {
+		this.stomach.push(animal);
+	} else {
+		throw new Error('cat can only eat mouse');
+	}
 };
 
 module.exports = cat;
